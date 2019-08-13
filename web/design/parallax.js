548f4629f6fd9de42c1ca6e1d647a11606b2c5c3
Blaineworld <blainephargett@gmail.com>
2019-08-12 23:08:45 -0500

    I'm doing web design stuff too - just put together a script that creates a parallax background effect!

---
+++ web/design/parallax.js
@@ -0,0 +1,42 @@
+'use strict';
+
+{
+	let e = document.scrollingElement;
+
+	const infinityRegex = /Infinity/g; // Regexes are objects, people!
+
+	var parallax = async function(v = e.scrollTop, h = e.scrollLeft) {
+		// Update the parallax effect.
+		let d = 0, x = "";
+		for await (d of layers)
+			x += -(h / d || 0) + "px " + -(v / d || 0) + "px,";
+		e.style.backgroundAttachment = "fixed";
+		e.style.backgroundPosition = await x.substr(0, x.length - 1).replace(infinityRegex, "0");
+	};
+
+	const meta = document.querySelector('meta[name="parallax-layers"]');
+
+	let layers = [];
+
+	if (meta)
+		layers = meta.content.split(" ");
+
+	Object.defineProperties(parallax, {
+		"element": {
+			"get": () => e,
+			"set": element => {
+				if (typeof element === "string")
+					e = document.querySelector(element) || e;
+				else
+					if (e instanceof Element)
+						e = e;
+			}
+		},
+		"layers": {
+			"get": () => Array.from(layers),
+			"set": depths => layers = Array.from(depths)
+		}
+	});
+
+	console.info("Parallax scrolling script made with 🌙 by Blaine Hargett.");
+}
