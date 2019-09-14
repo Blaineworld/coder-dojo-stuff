5a9dc43b7bbd522021429427c25531977b9f841d
Blaineworld <blainephargett@gmail.com>
2019-09-14 15:17:05 -0500

    Don't know if I've fully implemented all the new stuff, but a certain friend is being annoying.

--- web/libraries/minecraft-ui/minecraft-ui.js
+++ web/libraries/minecraft-ui/minecraft-ui.js
@@ -1,142 +1,203 @@
 'use strict';
 
 const MinecraftUI = (function() {
+	let sound = false;
+
 	const events = {
 		"press": new CustomEvent("press", {}),
 		"release": new CustomEvent("release", {}),
 		"unpress": new CustomEvent("unpress", {})
 	};
 
-	document.head.insertBefore(document.createElement("STYLE"), document.head.querySelector("style")).innerHTML = ":root{--mcui-pixel:1px}.mcui{background-size:calc(16*var(--mcui-pixel));display:inline-block;image-rendering:-moz-crisp-edges;image-rendering:-webkit-crisp-edges;image-rendering:pixelated;image-rendering:crisp-edges;margin:0;padding:0}.mcui-pressure-plate{background-color:silver;background-position:calc(var(--mcui-pixel)*-1) calc(var(--mcui-pixel)*-1);margin:var(--mcui-pixel);width:calc(var(--mcui-pixel)*14);height:calc(var(--mcui-pixel)*14)}.mcui-pressure-plate.mcui-active,.mcui-button.mcui-active{box-shadow: 0 0 calc(var(--mcui-pixel)*2) black inset}.mcui-button{background-color:silver;background-position:calc(var(--mcui-pixel)*-5) calc(var(--mcui-pixel)*-6);margin:calc(var(--mcui-pixel)*6) calc(var(--mcui-pixel)*5);width:calc(var(--mcui-pixel)*6);height:calc(var(--mcui-pixel)*4)}";
+	if (!document.head.querySelector("#mcui-stylesheet")) {
+		let e;
+
+		var fancyGraphics = e = document.head.insertBefore(document.createElement("LINK"), document.head.querySelector("style, link[rel=\"stylesheet\"]"));
+		e.href = "mcui/fancy.css";
+		e.id = "mcui-fancy-stylesheet";
+		e.rel = "stylesheet";
+
+		e = document.head.insertBefore(document.createElement("LINK"), document.head.querySelector("style, link[rel=\"stylesheet\"]"));
+		e.id = "mcui-stylesheet";
+		e.rel = "stylesheet";
+		e.href = "mcui/main.css";
+	}
 
 	const passive = { "passive": true };
 
 	const x = {};
 
-	const textures = {
-		"stone": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAAEnQAABJ0Ad5mH3gAAAC1SURBVDhPfZLLDcQgDAUplwJypwRKzuqxGjTxsjmM/MPPhqTNOW8zxriv61rg994fOdNw3ECuEiHEEscugTguQo0tBnsDXwFb82DRZmUXiGMRAfesDeqECqJcCZu+xwZ1in2wcFgCwY/ox+MgsXNLwFOTwOK7VonY4yu4eGp2jnzLunFYjTV9Jdd9JuwNjCe81WL3nwgUnI8fvBV2P+Kpgfhf7mcDDr3BQDi+QbAoh137+uP+AHnkd8OaodPkAAAAAElFTkSuQmCC",
-		"acacia_planks": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA1ElEQVQ4y4VTIRICQQzLcxgwDAaJxCBQOBSaf5xD4/gAH0DysWWyM9kJoYDodbftbJs0h8dp3e7HVaN/njcf9it+3S8aeNAjNAZvh+V4NPO6s4aGqlBeRbJ8pE/Az2U378az7tN21k05nT1OD2Fkh8SZMYfCHO9vEJSQdz4Squrh+LyDCK2IdF6QWJODzGcNfM/e5Z8WtEr46nLcXKF7cYSK3Wr3mR8TOMZKB55zTcjGBMmFOjhuceO1+KbxHLdaZ9eBF+Z/4Dqo+BkcJDbnwzmp9PECHCoz2n4Vz4IAAAAASUVORK5CYII=",
-		"birch_planks": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA00lEQVQ4T31SMQ4CMQzzV5l4AQzsTIiNGSYkJtZjZUPiAfecQiJcfL7AkKZNrDR2gvG+bY9h08KPz/3c/sSvp3VDAj5FwiJ4u6y+RS3Pd2DCUAHpCaJ5kewgjvNhmRZ3vo+7RRpzvGs8PMgxfnCes5hQydz7PaHABL3q4VSJh/LTHyhoJaTqAufqGnjeMV0D51jO3vTIDnR03q6PUD01QqVuNXvP9w6UY7UHmtOdoKHP27TgD+VeCBY+Y+fci4tWSgkKVABF+iXqZA+cm+qhmlT78QLNQ+ggcLCwKwAAAABJRU5ErkJggg==",
-		"dark_oak_planks": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAzklEQVQ4y31TMQ4CMQzzAxDSsYBuuInlJhATYmBnYuD/bylyJVfGBAZf2iRqYieH5+XQHqd9o31d5y/889+OUwMPeoSg877uxqMZ1505BKpEWSUJ+UjvgJ/zsu3gWfd13nQoprP7aSGOrJA80+dUGOP9g4ICsq5HUlU+nJ9XkKCVkK4LkmtqkPHMgc/Zq1SzTz16Bz66bDdH6FYaoVK3mn3GRwfOsdoDj/lOCNC8UwtVqPbCc/Frx7PdXHMBnpj/ge9Bpc/QILm5Hq5JtR9v52uBCq4zbNAAAAAASUVORK5CYII=",
-		"jungle_planks": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA0klEQVQ4y31TsQ0CQQzzRpQsgCiofwSKF1R0MAEdLSX9T0DHaId8kk/GBAp/7pLoEjt5PK/7tlymRvu6Hb/wz38/bBp40CMEnY/Tbjyacd2ZQ6BKlFWSkI/0Dvg5T+sOnnWft6sOxXR2Py3EkRWSZ/qcCmO8f1BQQNb1SKrKh/PzChK0EtJ1QXJNDTKeOfA5e5Vq9qlH78BHl+3mCN1KI1TqVrPP+OjAOVZ74DHfCQGad2qhCtVeeC5+7Xi2m2suwBPzP/A9qPQZGiQ318M1qfbjDQFvY4y3zmbRAAAAAElFTkSuQmCC",
-		"oak_planks": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA00lEQVQ4y31TMQ4CMQzzg9iZ7wFILMxIt/ABZiZ2FsSGeMKtzHysyJVcGRMYfGmTqImdHJ7XfVsuu0b7us9f+Oe/HacGHvQIQefjvBmPZlx35hCoEmWVJOQjvQN+TvO6g2fdD9tVh2I6u58W4sgKyTN9ToUx3j8oKCDreiRV5cP5eQUJWgnpuiC5pgYZzxz4nL1KNfvUo3fgo8t2c4RupREqdavZZ3x04ByrPfCY74QAzTu1UIVqLzwXv3Y82801F+CJ+R/4HlT6DA2Sm+vhmlT78QZ+7nLGaC/6FAAAAABJRU5ErkJggg==",
-		"spruce_planks": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA00lEQVQ4y31SMQ7CQAzzT1iYOyEkOrOxISGhbh26MyJGJn5d5JNcuSZlyOUusS6xE7zHfn4Nh5n+M51+7F98vHQzeNEnNAYf1275NPN6E0NDBaR/3o8LSJaftA54DOd9M971vvW7Zsrp7nF6iCMrJM+MORXm+F5RUELe9UiqwsP5eQUJWgnpuiC5pgaZTwx8zl6lmn3q0Trw0WW7OUL30giVutXsM7904ByrPfCc74QMmndqoQrVXjgWWzue7bpWTgkOdIBE2hJ1tQfJzfVwTar9+ALLJfdgEDC7owAAAABJRU5ErkJggg=="
+	const buttonTypes = [ "stone", "acacia", "birch", "dark_oak", "jungle", "oak", "spruce" ],
+		signTypes = [ "acacia", "birch", "dark_oak", "jungle", "oak", "spruce" ];
+
+	async function soundEnded() {
+		// When a sound is done playing.
+		await this.parentElement.removeChild(this);
+	}
+
+	async function play(file = "null", volume = 1.0, pitch = 1.0) {
+		// Play a sound.
+		if (!sound)
+			return;
+		const e = await document.head.appendChild(await document.createElement("audio"));
+		e.src = "mcui/sounds/" + file + ".ogg";
+		await e.addEventListener("ended", soundEnded, passive);
+		await e.addEventListener("error", soundEnded, passive);
+		e.autoplay = true;
+	}
+
+	async function setGraphics(setting) {
+		fancyGraphics.disabled = await await String(setting).toUpperCase() !== "FANCY";
+	}
+
+	async function deactivateElement(E, e) {
+		// Make an element not active anymore.
+		E.className = await E.className.replace("mcui-active", "");
+		await E.dispatchEvent(events.unpress);
+		if (e)
+			await play("random/click");
+	}
+
+	async function buttonMouseDown() {
+		// When you click on a button.
+		await clearTimeout(this.deactivateTimeout);
+		if (!await (" " + this.className + " ").includes(" mcui-active ")) {
+			this.className += " mcui-active";
+			await this.dispatchEvent(events.press);
+			await play("random/click");
+		}
+	}
+
+	async function buttonMouseEnter(event) {
+		// When your cursor enters a button.
+		if (event.buttons) {
+			await clearTimeout(this.deactivateTimeout);
+			if (!await (" " + this.className + " ").includes(" mcui-active ")) {
+				this.className += " mcui-active";
+				await this.dispatchEvent(events.press);
+				await play("random/click");
+			}
+		}
+	}
+
+	async function buttonMouseUp() {
+		// When you unclick on a button.
+		if (await (" " + this.className + " ").includes(" mcui-active ")) {
+			await clearTimeout(this.deactivateTimeout);
+			this.deactivateTimeout = await setTimeout(deactivateElement, 1000, this, "random/click");
+			await this.dispatchEvent(events.release);
+		}
+	}
+
+	const buttonProperties = {
+		"pressed": {
+			"get": function() {
+				return (" " + this.className + " ").includes(" mcui-active ");
+			},
+			"set": function(state) {
+				clearTimeout(this.deactivateTimeout);
+				this.className = this.className.replace("mcui-active", "");
+				if (state)
+					this.className += " mcui-active";
+			}
+		}
 	};
 
-	const buttonTextures = {
-		"STONE": "stone",
-		"ACACIA": "acacia_planks",
-		"BIRCH": "birch_planks",
-		"DARK_OAK": "dark_oak_planks",
-		"JUNGLE": "jungle_planks",
-		"OAK": "oak_planks",
-		"SPRUCE": "spruce_planks"
-	}, buttonTypes = [ "STONE", "ACACIA", "BIRCH", "DARK_OAK", "JUNGLE", "OAK", "SPRUCE" ], signTextures = {
-		"ACACIA": "acacia_sign",
-		"BIRCH": "birch_sign",
-		"DARK_OAK": "dark_oak_sign",
-		"JUNGLE": "jungle_sign",
-		"OAK": "oak_sign",
-		"SPRUCE": "spruce_sign"
-	}, signTypes = [ "ACACIA", "BIRCH", "DARK_OAK", "JUNGLE", "OAK", "SPRUCE" ];
-
-	function play(sound = "null", volume = 1.0, pitch = 1.0) {
-		// Play a sound.
-	}
-
-	function deactivateElement(E) {
-		// Make an element not active anymore.
-		E.className = E.className.replace("mcui-active", "");
-		E.dispatchEvent(events.unpress);
-	}
-
-	function buttonMouseDown() {
-		// When you click on a button.
-		clearTimeout(this.deactivateTimeout);
-		if (!(" " + this.className + " ").includes(" mcui-active ")) {
-			this.className += " mcui-active";
-			this.dispatchEvent(events.press);
-		}
-	}
-
-	function buttonMouseEnter(event) {
-		// When your cursor enters a button.
-		clearTimeout(this.deactivateTimeout);
-		if (event.buttons)
-			if ((" " + this.className + " ").includes(" mcui-active ")) {
-				this.className += " mcui-active";
-				this.dispatchEvent(events.press);
-			}
-	}
-
-	function buttonMouseUp() {
-		// When you unclick on a button.
-		if ((" " + this.className + " ").includes(" mcui-active ")) {
-			clearTimeout(this.deactivateTimeout);
-			this.deactivateTimeout = setTimeout(deactivateElement, 1000, this);
-			this.dispatchEvent(events.release);
-		}
-	}
-
 	Object.defineProperties(x, {
+		"version": {
+			"value": "alpha-rd",
+			"enumerable": true
+		},
+		"graphics": {
+			"enumerable": true,
+			"get": function() {
+				if (fancyGraphics.disabled)
+					return "Fast";
+				return "Fancy";
+			},
+			"set": function(setting) {
+				setTimeout(setGraphics, 0, setting);
+				if (document.readyState !== "complete")
+					addEventListener("load", () => setGraphics(setting));
+			}
+		},
+		"sound": {
+			"enumerable": true,
+			"get": function() {
+				if (sound)
+					return "On";
+				return "Off";
+			},
+			"set": function(setting) {
+				sound = String(setting).toUpperCase() === "ON";
+			}
+		},
 		"createButton": {
-			"value": function CREATE_BUTTON(type = "STONE", container = document.body) {
+			"value": function CREATE_BUTTON(type = "stone", container = document.body) {
 				// Create an HTML Minecraft button.
-				let v = String(type).toUpperCase();
-				if (!(v in buttonTextures))
-					if (v === "RANDOM")
+				let v = String(type).toLowerCase();
+				if (!buttonTypes.includes(v))
+					if (v === "random")
 						v = buttonTypes[Math.floor(Math.random() * buttonTypes.length)];
 					else
 						throw Error("Unknown button variant: " + v);
+				if (v !== "stone")
+					v += "_planks";
 				const e = document.createElement("DIV");
 				e.className = "mcui mcui-button";
-				e.style.backgroundImage = "url('" + textures[buttonTextures[v]] + "')";
+				e.style.backgroundImage = "url('mcui/textures/block/" + v + ".gif')";
 				e.addEventListener("mousedown", buttonMouseDown, passive);
 				e.addEventListener("mouseup", buttonMouseUp, passive);
 				e.addEventListener("mouseenter", buttonMouseEnter, passive);
 				e.addEventListener("mouseleave", buttonMouseUp, passive);
 				if (container instanceof Element)
 					container.appendChild(e);
+				Object.defineProperties(e, buttonProperties);
 				return e;
 			}
 		},
 		"createPressurePlate": {
-			"value": function CREATE_PRESSURE_PLATE(type = "STONE", container = document.body) {
+			"value": function CREATE_PRESSURE_PLATE(type = "stone", container = document.body) {
 				// Create an HTML Minecraft pressure plate.
-				let v = String(type).toUpperCase();
-				if (!(v in buttonTextures))
-					if (v === "RANDOM")
+				let v = String(type).toLowerCase();
+				if (!buttonTypes.includes(v))
+					if (v === "random")
 						v = buttonTypes[Math.floor(Math.random() * buttonTypes.length)];
 					else
 						throw Error("Unknown pressure plate variant: " + v);
+				if (v !== "stone")
+					v += "_planks";
 				const e = document.createElement("DIV");
 				e.className = "mcui mcui-pressure-plate";
-				e.style.backgroundImage = "url('" + textures[buttonTextures[v]] + "')";
+				e.style.backgroundImage = "url('mcui/textures/block/" + v + ".gif')";
 				e.addEventListener("mouseenter", buttonMouseDown, passive);
 				e.addEventListener("mouseleave", buttonMouseUp, passive);
 				if (container instanceof Element)
 					container.appendChild(e);
+				Object.defineProperties(e, buttonProperties);
 				return e;
 			}
 		},
-		"createSign": {
-			"value": function CREATE_SIGN(type = "OAK", container = document.body) {
+		"createWallSign": {
+			"value": function CREATE_WALL_SIGN(type = "oak", container = document.body) {
 				// Create an HTML Minecraft sign.
-				let v = String(type).toUpperCase();
-				if (!(v in signTypes))
-					if (v === "RANDOM")
+				let v = String(type).toLowerCase();
+				if (!signTypes.includes(v))
+					if (v === "random")
 						v = signTypes[Math.floor(Math.random() * signTypes.length)];
 					else
 						throw Error("Unknown sign variant: " + v);
-				const e = document.createElement("DIV");
-				e.className = "mcui mcui-sign";
-				e.style.backgroundImage = "url('" + textures[signTextures[v]] + "')";
+				const e = document.createElement("PRE");
+				e.className = "mcui mcui-wall-sign";
+				e.style.backgroundImage = "url('mcui/textures/entity/signs/custom/" + v + ".gif')";
 				if (container instanceof Element)
 					container.appendChild(e);
 				return e;
 			}
 		}
 	});
+
+	addEventListener("load", () => fancyGraphics.disabled = true);
 
 	return x;
 })();
